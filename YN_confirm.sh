7742f04ed4cab874cbfaae5ffd8701241f4e4039
Denis Dechev <denisde4ev@vivaldi.net>
2022-01-08 16:45:26 +0200

    flip arguments $1 is the default option, add --help and --help-long message

--- YN_confirm.sh
+++ YN_confirm.sh
@@ -1,59 +1,127 @@
 #!/bin/sh
 
-# USAGE: YN_confirm [prompt text] [default when no input] [default when wrong input]
-# example: YN_confirm 'Do somethhing harmless?' Y # will exit-stat 0, exept when user speciy [nN]*
-# example: YN_confirm 'Do somethhing recomended?' y # will exit-stat 0 exept when user gives .....???????
-# example: YN_confirm 'Do somethhing recomended?' Yes # will exit-stat 5 exept when ?????....
+
 YN_confirm() {
 
-	local yn_prompt;yn_prompt='[y/n]'
-	case ${2-} in
-		[Yy]|[Yy]es) yn_prompt='[Y/n]';;
-		[Nn]|[Nn]o) yn_prompt='[y/N]';;
-		'') ;;
-		*) printf %s\\n >&2 "YN_confirm: ignoring bad argument '$2'";;
+
+	io_file=/dev/tty
+	case $1 in
+	--help|--help-long)
+		case $1 in
+			--help-long) help_long=1;;
+			*) unset help_long;;
+		esac
+		printf %s\\n \
+			"Usage: YN_confirm [--stdin] [default] [...prompt text]" \
+			"" \
+			"Options:" \
+			"  --help       display help message and exit" \
+			"  --help-long  display long help message with argument examples and exit" \
+			"  --stdin      read response from standard input" \
+			"" \
+			"Arguments:" \
+			"  [...prompt text]  is joined by new lines" \
+			"  [default]:" \
+			"    I. The length of [default] argument matters" \
+			"    when \`read\` command failed" \
+			"      1 char|'' => exit code 5" \
+			"      2+ chars  => exit code 0" \
+			${help_long+ \
+			"" \
+			"      1. when \`read\` failed and [default] in [Yy]'es'    => exit code 0" \
+			"      2. when \`read\` failed and [default] in [Nn]'o'     => exit code 0 (counterintuitive?)" \
+			"      3. when \`read\` failed and [default] in [Mm]'aybe'  => exit code 0" \
+			"      3. when \`read\` failed and [default] in ''          => exit code 5" \
+			"      4. when \`read\` failed and [default] in [Yy]        => exit code 5" \
+			"      5. when \`read\` failed and [default] in [Nn]        => exit code 5" \
+			"" \
+			} \
+			"    II. The capitalization of [default] argument matters" \
+			"    when input is not as expected" \
+			"      upper    => exit code 0" \
+			"      lower|'' => exit code 3" \
+			${help_long+ \
+			"" \
+			"      1. when \$response not in [YyNn]*|'' and [default] in 'Y'*       => exit code 0" \
+			"      1. when \$response not in [YyNn]*|'' and [default] in 'N'*       => exit code 0" \
+			"      1. when \$response not in [YyNn]*|'' and [default] in 'Maybe'    => exit code 0" \
+			"      1. when \$response not in [YyNn]*|'' and [default] in ''|'maybe' => exit code 3" \
+			"      1. when \$response not in [YyNn]*|'' and [default] in 'y'*       => exit code 3" \
+			"      1. when \$response not in [YyNn]*|'' and [default] in 'n'*       => exit code 3" \
+			} \
+			${help_long+ \
+			"Exit status:" \
+			"  0 =>  answer is Yes" \
+			"  1 =>  answer is No" \
+			"  2 =>  no answer and no default answer specified" \
+			"  3 =>  not expected answer" \
+			`: " 130    Interrupted with CTRL-C or ESC" ` \
+			} \
+		;
+		exit
+		;;
+	--stdin)
+		shift
+		unset io_file
+		;;
 	esac
 
-	printf %s "${1-}${1:+ }$yn_prompt "
 
-	local respond;respond=''
-	read respond </dev/tty || {
-		case ${2:-} in [Yy]es) return 0; esac
-		return 5
+	default=${1-}; shift
+	local yn_prompt;yn_prompt='[y/n]'
+	case $default in
+		[Yy]|[Yy]es|YES) yn_prompt='[Y/n]';;
+		[Nn]|[Nn]o|NO)   yn_prompt='[y/N]';;
+		[Mm]aybe) ;;
+		'') ;;
+		*) printf %s\\n >&2 "YN_confirm: warning, bad [default] argument '$1'";;
+	esac
+
+	case ${io_file+x} in
+		x) printf %s "${@-}${@:+ }$yn_prompt" >"$io_file";;
+		*) printf %s "${@-}${@:+ }$yn_prompt" >&2;;
+	esac
+
+	# todo: integrate this exit status
+	unset respond
+	case ${io_file+x} in
+		x) read respond <"$io_file";;
+		*) read respond;;
+	esac || {
+		case $default in
+			[Yy]es|[Nn]o) return 0;;
+			*) return 5;;
+		esac
 	}
 
-	# When cant read:
-	# read respond  || \
-		# if [ "$2" = y ] || [ "$2" = Y ]
-		# then return 0
-		# else return 4
-		# fi
-		## For now fail to read  is not important,
-		## still will take default option, respond remains '' 
-	
-	
+
+
 	case $respond in
 		[Yy]*) return 0;;
 		[Nn]*) return 1;;
 		'')
-			case ${2:-} in [Yy]*) return 0; esac
-			return 2
+			case $default in
+				[Yy]*) return 0;;
+				[Nn]*) return 1;;
+				*) return 2;;
+			esac
 		;;
 		*)
-			case ${2:-} in [YN]*) return 0; esac
-			return 3
+			case $default in
+				[YN]*) return 0;;
+				*) return 3;;
+			esac
 		;;
 	esac
 }
 
 case "${0##*/}" in
-	YN_confirm|YN_confirm.sh) YN_confirm "$@";;
+	YN_confirm|YN_confirm[!A-Za-z0-9_]*) YN_confirm "$@";;
 	*) ;;
 esac
 
 
 
-#
 #""" # bat -r23:38 /usr/share/yash/initialization/common
 #  23   │   history()
 #  24   │   if [ -t 0 ] && (
